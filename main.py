185ee6ad0f66aa056a89d690298c494cc54daeab
BaronFrancois <francix_88@hotmail.fr>
2023-11-09 22:28:05 +0100

    1/The script is working for every data specified into 'notes' 2/The script is creating a csv file that is readable (/!\ Some caracters doesn't work even with utf setings) 3/Adding some comments to explain my code 4/Addingg data I need & deleting data I don't 5/Changing the caracters for review rating

--- main.py
+++ main.py
@@ -1,93 +1,123 @@
-import requests
-from bs4 import BeautifulSoup
-# join Url from website image relative path
-from urllib.parse import urljoin
-import json
-import csv
+# Importing required modules
+import re  # For regular expressions
+import requests  # For making HTTP requests
+from bs4 import BeautifulSoup  # For parsing HTML
+from urllib.parse import urljoin  # For joining URLs
+import csv  # For writing to CSV files
 
-url = "https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
+# Function to extract the category from the HTML soup
+def extract_category(soup):
+    # Find the breadcrumb trail in the page, which usually contains the category
+    breadcrumb = soup.find('ul', class_='breadcrumb')
+    # If breadcrumb is found, proceed to find the category
+    if breadcrumb:
+        # The category is typically the second to last item in the breadcrumb
+        categories = breadcrumb.find_all('li')
+        category = categories[-2].get_text().strip() if len(categories) > 2 else 'No category'
+        return category
+    else:
+        return 'No category'  # If breadcrumb is not found, return 'No category'
 
-# 
-response = requests.get(url)
+# Function to extract the review rating from the HTML soup
+def extract_review_rating(soup):
+    # Find the 'star-rating' class to get the review rating
+    rating_element = soup.find('p', class_='star-rating')
+    if rating_element:
+        # The class attribute of the rating element contains the rating as the second class (e.g., 'Three')
+        rating_text = rating_element.get('class')[1] if len(rating_element.get('class')) > 1 else "No rating"
+        # Map the textual rating to a number
+        rating_conversion = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
+        return rating_conversion.get(rating_text, "No rating")
+    else:
+        return "No rating"  # If no rating element is found, return 'No rating'
 
-soup = BeautifulSoup(response.text, 'html.parser')
+# Function to scrape individual book details from its page
+def scrape_book(url):
+    # Send a request to the book page and get the response content
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-table = soup.find_all('tr')
+    # Initialize a dictionary to store book details
+    product_data = {
+        "Product Page URL": url,  # URL of the book's product page
+        "Title": soup.find('h1').text.strip(),  # Title of the book
+        "Product Description": "No description available",  # Default if no description is found
+        "Image URL": urljoin(url, soup.find('img')['src']) if soup.find('img') else "",  # Image URL
+        "Category": extract_category(soup),  # Category of the book
+        "Review Rating": extract_review_rating(soup)  # Review rating
+    }
 
-# URL PageProduct
-product_page_url = url
+    # Find the product description paragraph following the 'product_description' element
+    description_element = soup.find(id='product_description')
+    if description_element:
+        product_data["Product Description"] = description_element.find_next_sibling('p').text.strip()
 
-# Title
-title = soup.find('h1').text
+    # Extract additional product information from the product page table
+    table = soup.find_all('tr')
+    for row in table:
+        th = row.find('th')
+        td = row.find('td')
+        if th and td:
+            key = th.text.strip()
+            value = td.text.strip()
+            if key == 'UPC':
+                product_data["Universal Product Code (UPC)"] = value
+            elif key == 'Price (excl. tax)':
+                product_data["Price Excluding Tax"] = value.replace('Â£', '£')
+            elif key == 'Price (incl. tax)':
+                product_data["Price Including Tax"] = value.replace('Â£', '£')
+            elif key == 'Availability':
+                match = re.search(r'(\d+) available', value)
+                product_data["Number Available"] = match.group(1) if match else "0"
 
-# Description product
-description_element = soup.find(id='product_description')
-# If there is no escription
-if description_element:
-    product_description = description_element.find_next_sibling('p').text
-else:
-    product_description = "No description available"
+    # Return the dictionary containing all the scraped data
+    return product_data
 
-# image URL
-image_element = soup.find('img')
-# Relative Path for image URL
-image_url = urljoin(url, image_element['src'])
+# Function to scrape the listing page and get all book URLs
+def scrape_listing_page(url):
+    # Send a request to the listing page and get the response content
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
+    # Find all book entries on the page
+    books = soup.find_all('article', class_='product_pod')
 
-product_data = {
-    "Product Page URL": product_page_url,
-    "Title": title,
-    "Product Description": product_description,
-    "Image URL": image_url
-}
+    # List to store URLs of individual book pages
+    book_urls = [urljoin(url, book.find('h3').find('a')['href']) for book in books]
 
-data = product_data
+    # Return the list of URLs
+    return book_urls
 
-csv_file = "output.csv"
+# Main script execution
+if __name__ == "__main__":
+    # URL of the page with book listings
+    listing_url = "https://books.toscrape.com/catalogue/category/books_1/index.html"
+    # Scrape the listing page for book URLs
+    book_urls = scrape_listing_page(listing_url)
 
-for row in table:
-    td = row.find_all('td')
-    th = row.find_all('th')
-    print(row.text.strip())
-    product_data[th[0].text.strip()] = td[0].text.strip()
+    # List to store data of all books
+    all_book_data = []
 
+    # Loop through each book URL to scrape information
+    for url in book_urls:
+        try:
+            # Scrape data for the current book
+            book_data = scrape_book(url)
+            # Add the scraped data to the list
+            all_book_data.append(book_data)
+            # Print the title of the book that was just scraped
+            print(f"Scraped data for {book_data['Title']}")
+        except Exception as e:
+            # Print an error message if scraping fails
+            print(f"Failed to scrape data for url {url}: {e}")
 
+    # Write the scraped data to a CSV file
+    csv_file = "books_data.csv"
+    with open(csv_file, mode="w", newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=all_book_data[0].keys())
+        writer.writeheader()
+        for book_data in all_book_data:
+            writer.writerow(book_data)
 
-# Utilisation d'une boucle pour afficher les données
-# for key, value in product_data.items():
-#     print(f"{key}: {value}")
-
-# with open('product_data.json', 'w') as json_file:
-#     json.dump(product_data, json_file, indent=4)
-
-with open(csv_file, mode="w", newline='', encoding ='utf8') as file:
-    writer = csv.writer(file) #what does writer do
-    writer.writerow(data.keys())
-    print(f"{data.values()} DATAAAAAAAAAAA")
-
-    writer.writerow(data.values())
-
-
-
-
-# my_dict = {'key1': 'value1', 'key2': 'value2'}, you can access 'value1' by using my_dict['key1'].
-
-
-my_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
-
-# Iterating over keys
-# for key in my_dict:
-#     print(key)
-
-
-# Iterating over values
-# for value in my_dict.values():
-#     print(value)
-
-
-# Iterating over key-value pairs
-# for key, value in my_dict.items():
-#     print(f"Key: {key}, Value: {value}")
-
-
-# Nest step is to add category function + create a function to use it in category "filter"
+    # Print a message indicating the completion of the CSV writing process
+    print(f"Data for all books has been written to {csv_file}")
